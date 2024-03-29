a2a8c2647463533d837928d1b7d8b31a1dd0dddc
Mateus Macedo <84480587+macedocedo@users.noreply.github.com>
2024-03-29 12:17:35 -0300

    Update Rastromouse.py

--- Rastromouse.py
+++ Rastromouse.py
@@ -5,6 +5,7 @@ def iniciar_selecao():
     label_instrucao.config(text="Posicione o cursor...")
     botao_selecionar.config(state=tk.DISABLED)
     janela.after(3000, capturar_posicao)  # Aguarda 3 segundos antes de capturar a posição
+    
 
 def capturar_posicao():
     posicao = pyautogui.position()
@@ -18,7 +19,7 @@ janela.title("Seleção de Posição do Cursor")
 
 # Criar rótulo de instrução
 label_instrucao = tk.Label(janela, text="Clique em 'Selecionar Posição' e posicione o cursor.")
-label_instrucao.pack(pady=10)
+label_instrucao.pack(pady=5)
 
 # Botão para iniciar seleção de posição
 botao_selecionar = tk.Button(janela, text="Selecionar Posição", command=iniciar_selecao)
@@ -26,11 +27,7 @@ botao_selecionar.pack(pady=5)
 
 # Rótulo para exibir a posição
 label_posicao = tk.Label(janela, text="")
-label_posicao.pack(pady=10)
-
-# Botão para fechar a janela
-botao_fechar = tk.Button(janela, text="Fechar", command=janela.quit)
-botao_fechar.pack(pady=5)
+label_posicao.pack(pady=10, padx=20)
 
 # Loop principal da janela
 janela.mainloop()
